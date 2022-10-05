e94c9cdf295860ea6b11096cba34be1f65433a0d
Yawar <mybmscience@gmail.com>
2022-10-05 22:46:57 +0530

    program goals and outline
    laid outline of goals

---
+++ sigmain.c
@@ -0,0 +1,34 @@
+/*
+
+sigLib - A simple signal processing library
+
+This library provides simple signal processing capability which can be integrated in microcontroller or other application environments.
+
+This library contains following functions;
+  - Filters with circular buffers | order adjusted
+    - based on custom impulse response
+    - Low pass
+    - High pass
+    - Band pass
+    - Band stop
+    - average
+  - PID controller | discrete
+    - P
+    - PI
+    - PI & D
+    - PID
+  
+  
+  
+  ---> later
+  
+    mpc
+    lqr
+    feedforward
+    fopid
+    ? kalman integration ??
+  ----
+  
+  
+
+*/
